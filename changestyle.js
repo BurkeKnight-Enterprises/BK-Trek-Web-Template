531a7c73ade130fcb8b599c48698827065bcad62
Burke Knight <github@burkeknight.com>
2015-05-30 19:49:25 -0400

    Initial Upload

---
+++ changestyle.js
@@ -0,0 +1,14 @@
+function ChangeColor(Color) {
+if (Color == 'Enterprise') {
+	document.getElementById("logo").style.background="url(images/l1.gif) no-repeat";
+}
+if (Color == 'Defiant') {
+	document.getElementById("logo").style.background="url(images/l4.gif) no-repeat";
+}
+if (Color == 'Voyager') {
+	document.getElementById("logo").style.background="url(images/l2.gif) no-repeat";
+}
+if (Color == 'Prometheus') {
+	document.getElementById("logo").style.background="url(images/l3.gif) no-repeat";
+}
+}
